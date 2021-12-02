fe9f0bfe2cd49831e05dd68a4d91b37f6fb834be
blylei <blylei.info@gmail.com>
2021-12-02 21:18:25 +0800

    init project

--- web/main/__init__.py
+++ web/main/__init__.py
@@ -2,3 +2,14 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 # This file is part of frabit-server
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+from . import views, errors
+from ..models import Permission
+
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
