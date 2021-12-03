8750968836badd5df42c127ff3516f01cbcf43d2
blylei <blylei.info@gmail.com>
2021-12-03 12:21:37 +0800

    init project

--- web/display/__init__.py
+++ web/display/__init__.py
@@ -1,4 +1,4 @@
-# (c) 2020 frabit-web Project maintained and limited by FrabiTech < blylei.info@gmail.com >
+# (c) 2021 frabit-web Project maintained and limited by FrabiTech < blylei.info@gmail.com >
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
-# This file is part of frabit-server
+# This file is part of frabit-web
