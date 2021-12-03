9417922f6ee7557a499740e73f0b7e05ef1fcb54
blylei <blylei.info@gmail.com>
2021-12-03 12:20:43 +0800

    init project

--- web/main/__init__.py
+++ web/main/__init__.py
@@ -1,7 +1,8 @@
-# (c) 2020 Dredis Project maintained and limited by Blylei < blylei.info@gmail.com >
+# (c) 2021 frabit-web Project maintained and limited by FrabiTech < blylei.info@gmail.com >
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
-# This file is part of frabit-server
+# This file is part of frabit-web
+
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
