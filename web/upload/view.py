40f6d726c70f6dd5b09e7233bb9788e6ead177cd
blylei <blylei.info@gmail.com>
2021-12-06 11:31:01 +0800

    init project

--- web/upload/view.py
+++ web/upload/view.py
@@ -2,3 +2,8 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 # This file is part of frabit-web
+
+"""
+Uplode a License for enterprise user, default open source version unnecessary
+"""
+
